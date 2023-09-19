da87c674e31ef64c0e01ebefd97f54c3187a8741
zanetamak <zaneta.zawislak@gmail.com>
2023-09-19 12:50:26 +0200

    task1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,7 +1,8 @@
 'use strict';
 const body = document.querySelector('body');
-const bthStart = document.querySelector('[data-start]');
-const bthStop = document.querySelector('[data-stop]');
+const bthStart = document.querySelector('button[data-start]');
+// czasem kilka elementów w bloku moze mieć np data-start dlatego warto zawęzić wyszukiwanie elementu
+const bthStop = document.querySelector('nutton[data-stop]');
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
@@ -18,6 +19,7 @@ let timerId = null;
 const startChange = () => {
     bthStart.disabled = true;
     bthStop.disabled = false;
+        colorChange();
     timerId = setInterval(() => {
         colorChange();
     }, 2000);
